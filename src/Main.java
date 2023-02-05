975e3d95be065f50fc22ff482fc29c3679e85628
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:32:47 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HavingSuperAbility superAbilities[]={new Warrior(),new Magic(),new Medic()};
+        for (int i = 0; i < superAbilities.length; i++) {
+            superAbilities[i].applySuperAbility("lOVE");
+            superAbilities[i].applySuperAbility("RESPECT");
+            superAbilities[i].applySuperAbility("FREEDOM");
+        }
     }
 }
