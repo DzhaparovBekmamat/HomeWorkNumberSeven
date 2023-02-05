ead928b7ba33ab087285495c91ee8932b7ca0c85
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:31:47 +0600

    Update Medic.java

--- src/Medic.java
+++ src/Medic.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Medic {
+public class Medic extends Hero implements HavingSuperAbility{
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Medic применил суперспособность - " + superAbilityType);
+    }
 }
