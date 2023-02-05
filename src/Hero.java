5935f87ea650ea01d4eae367a91b33d3722452a1
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:29:09 +0600

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Hero {
+public class Hero implements HavingSuperAbility{
+    public int health;   // зделал public  потому что в задании не было про private, не ругайте меня (я хотел сократить как можно быльше коды)
+    public int damage;
+    public String superAbility;
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("fire "+superAbilityType);
+    }
 }
